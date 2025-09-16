2b9261c59aa02fcdde0fbe597e10847bc92db9fe
hadibourji1 <hadibourji1@gmail.com>
2025-09-16 13:07:55 -0500

    Create repo, create script to play video, create script to count bugs

---
+++ play_video.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import vlc
+
+root = tk.Tk()
+root.title("VLC Player")
+
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+media = instance.media_new("Basler_boA8100-16cc__40578090__20250813_172842739.mp4")
+player.set_media(media)
+
+# Video frame
+video_frame = tk.Frame(root)
+video_frame.pack(fill="both", expand=1)
+player.set_hwnd(video_frame.winfo_id())
+
+
+# Start playback
+player.play()
+root.mainloop()
